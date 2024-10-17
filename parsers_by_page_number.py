98bc1b33d609bd1d2290c8308247026384f32917
NirAharon1 <niraharon8007@gmail.com>
2024-10-18 00:25:05 +0300

    update all function in parsers_by_page.py and parser_to_list.py

--- parsers_by_page_number.py
+++ parsers_by_page_number.py
@@ -1,60 +1,47 @@
+"""
+This module provides functions for parsing PDF files and extracting text from specific pages using various libraries.
+The functions in this module allow users to extract text from PDF pages using different parsing methods.
+Each function is designed to handle a specific parsing library and return the extracted text for a given page number.
+"""
+
+import io
 import pypdf
 import pymupdf
-from decorators import time_execution
 from pdf2image import convert_from_path
-from PIL import Image
 import pytesseract
-import numpy as np
-import cv2 as cv
 import pymupdf4llm
 import aspose.words as aw
-import io
-
+from decorators import time_execution
 
 
 @time_execution
 def pypdf_parse_page(pdf_path: str, page_number: int) -> str:
     with open(pdf_path, "rb") as f:
-        text = ""
-        reader = pypdf.PdfReader(f,strict=True)
+        reader = pypdf.PdfReader(f, strict=True)
         page = reader.pages[page_number]
-        text += page.extract_text(space_width=100)
-        text = text[0:230000] #limiting the text size
+        text = page.extract_text(space_width=100)[:230000]  # limiting the text size
     return text
 
 
 @time_execution
 def pymupdf_parse_page(pdf_path: str, page_number: int) -> str:
-    text = ""
-    file = pymupdf.open(pdf_path)
-    page = file.load_page(page_number)
-    text += page.get_text()
-    text = text[0:230000] #limiting the text size
+    with pymupdf.open(pdf_path) as file:
+        page = file.load_page(page_number)
+        text = page.get_text()[:230000]  # limiting the text size
     return text
-
 
 @time_execution
 def pymupdf4llm_parse_page(pdf_path: str, page_number: int) -> str:
-    text = pymupdf4llm.to_markdown(pdf_path, 
-                                   write_images=False,
-                                   pages=[page_number],
-                                   )
-    return text
+    return pymupdf4llm.to_markdown(pdf_path, write_images=False, pages=[page_number])
+
 
 
 @time_execution
 def pytesseract_parse_page(pdf_path: str, page_number: int) -> str:
-    text = ''
-    images = convert_from_path(pdf_path, dpi=300, first_page=page_number, last_page=page_number)
-    if images:
-        image = images[0]
-        image_cv = np.array(image)  
-        image_cv = cv.cvtColor(image_cv, cv.COLOR_BGR2GRAY)
-        image = Image.fromarray(image_cv)
-        text = pytesseract.image_to_string(image, lang='heb+eng') 
-        text = text[0:230000] #limiting the text size
+    if images := convert_from_path(pdf_path, dpi=300, first_page=page_number, last_page=page_number):
+        image = images[0].convert('L')  # Convert directly to grayscale
+        text = pytesseract.image_to_string(image, lang='heb+eng')[:230000]  # limiting the text size
     return text
-
 
 
 @time_execution
@@ -63,13 +50,10 @@ def aspose_parse_page(pdf_path: str, page_number: int) -> str:
     load_options.page_index = page_number
     load_options.page_count = 1
     doc = aw.Document(pdf_path, load_options)
-    save_options = aw.saving.MarkdownSaveOptions()   
+    save_options = aw.saving.MarkdownSaveOptions() 
     save_options.images_folder = "aspose_temp_images"
 
     with io.BytesIO() as stream:
         doc.save(stream, save_options)
         stream.seek(0)# Convert the stream to a string
-        text = stream.read().decode('utf-8')
-        return text
-    
-
+        return stream.read().decode('utf-8')
--- parsers_to_list.py
+++ parsers_to_list.py
@@ -1,15 +1,13 @@
-import pypdf
-import pymupdf
-import cv2 as cv
-from pdf2image import convert_from_path
-import numpy as np
-import pytesseract
-from PIL import Image
-from decorators import time_execution
-from llama_parse import LlamaParse
-from dotenv import load_dotenv
 import os
 import shutil
+import pypdf
+import pymupdf
+from pdf2image import convert_from_path
+import pytesseract
+from llama_parse import LlamaParse
+from dotenv import load_dotenv
+from decorators import time_execution
+
 
 load_dotenv()
 llama_cloud_api_key = os.getenv("LLAMA_CLOUD_API_KEY")
@@ -17,45 +15,23 @@ llama_cloud_api_key = os.getenv("LLAMA_CLOUD_API_KEY")
 
 @time_execution
 def pypdf_parse_to_list(pdf_path: str) -> list[str]:
-    page_list = []
     with open(pdf_path, "rb") as f:
-        file = pypdf.PdfReader(f,strict=True)
-        for page in file.pages:
-            text = page.extract_text(space_width=100)
-            text = text[0:230000] #limiting the text size
-            page_list.append(text)
-    return page_list
-
+        file = pypdf.PdfReader(f, strict=True)
+        return [page.extract_text(space_width=100)[:230000] for page in file.pages]
 
 @time_execution
 def pymupdf_parse_to_list(pdf_path: str) -> list[str]:
-    page_list = []
-    file = pymupdf.open(pdf_path)
-    for page_num in range(len(file)):
-        page = file.load_page(page_num)
-        text = page.get_text()
-        text = text[0:230000] #limiting the text size
-        page_list.append(text)
-    return page_list
+    with pymupdf.open(pdf_path) as file:
+        return [file.load_page(page_num).get_text()[:230000] for page_num in range(len(file))]
+    
 
 @time_execution
 def pytesseract_parse_to_list(pdf_path: str, dpi:int=200) -> list[str]:
-    page_list = []
     images = convert_from_path(pdf_path, dpi=dpi, fmt='jpeg', poppler_path=shutil.which('pdfinfo'))
-    for i, image in enumerate(images):
-        image_cv = np.array(image)
-        image_cv = cv.cvtColor(image_cv, cv.COLOR_BGR2GRAY)
-        image = Image.fromarray(image_cv)
-        text = pytesseract.image_to_string(image, lang='heb+eng') 
-        text = text[0:230000] #limiting the text size
-        page_list.append(text)
-    return page_list
+    return [pytesseract.image_to_string(image.convert('L'), lang='heb+eng')[:230000] for image in images]
 
 
 @time_execution
 def llama_parse_to_list(pdf_path: str) -> str:
-    parser = LlamaParse(result_type="markdown",
-                        api_key=llama_cloud_api_key,
-                        )
-    document = parser.load_data(pdf_path)
-    return document
+    parser = LlamaParse(result_type="markdown", api_key=llama_cloud_api_key)
+    return parser.load_data(pdf_path)
